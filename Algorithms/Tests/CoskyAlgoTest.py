703994f9f0ca2b759a39239d7e8ae13487b9cfeb
Turlure-Nael-23018992 <146089336+Turlure-Nael-23018992@users.noreply.github.com>
2025-04-10 14:09:35 +0200

    ajout tests Premiers algo

--- Algorithms/Tests/CoskyAlgoTest.py
+++ Algorithms/Tests/CoskyAlgoTest.py
@@ -1,37 +1,54 @@
 import unittest
 import json
+from collections import OrderedDict
 
 from Algorithms.CoskyAlgorithme import CoskyAlgorithme
 
+
 class CoskyAlgoTest(unittest.TestCase):
+    """
+    Test class for the Cosky algorithm.
+    """
 
     def setUp(self):
+        """
+        Setup method to initialize the CoskyAlgorithme instance.
+        """
         with open('../r_big.json', 'r') as f:
             r_big_loaded = json.load(f)
 
         # Convert the loaded dictionary values to tuples
         self.r_big_reloaded = {key: tuple(value) for key, value in r_big_loaded.items()}
 
+        # Expected values with keys converted to integers
+        self.expected_values = {
+            103: [1, 63, 0.0886754904499806, 0.5563069153560654],
+            150: [1, 81, 0.0238015221847852, 0.744486440507893],
+            32: [5, 68, 0.00101601862031575, 0.9881951629372444],
+            382: [5, 16, 0.0290286872591493, 0.8536447098741473],
+            399: [1, 37, 0.105243370873928, 0.3925627936821398],
+            496: [1, 10, 0.123358262237618, 0.18326805476129948],
+            541: [6, 113, 0.000471306256019766, 0.953194312519765],
+            551: [6, 13, 0.0569350631190667, 0.7448746655457289],
+            593: [2, 15, 0.0582544098560979, 0.5406510449527204],
+            604: [8, 11, 0.0481795194440872, 0.7757600377716273],
+            67: [2, 17, 0.00315426440199051, 0.9950462086984445],
+            89: [3, 11, 0.0714556428342785, 0.5268573236431924]
+        }
+
+        # Run CoskyAlgorithme and sort the output dictionary
+        self.cosky_algo = CoskyAlgorithme(self.r_big_reloaded).s
+        # Convert the keys to integers
+        self.cosky_algo = {int(k): v for k, v in self.cosky_algo.items()}
+        print(self.cosky_algo)  # Vérification
 
     def test_cosky_algo(self):
         """
         Test the Cosky algorithm with a sample dataset.
         """
-        expected_values = {32: [5, 68, 0.00101601862031575, 0.9881951629372444],
-                           67: [2, 17, 0.00315426440199051, 0.9950462086984445],
-                           89: [3, 11, 0.0714556428342785, 0.5268573236431923],
-                           103: [1, 63, 0.0886754904499806, 0.5563069153560651],
-                           150: [1, 81, 0.0238015221847852, 0.7444864405078929],
-                           382: [5, 16, 0.0290286872591493, 0.8536447098741473],
-                           399: [1, 37, 0.105243370873928, 0.39256279368213964],
-                           496: [1, 10, 0.123358262237618, 0.18326805476129943],
-                           541: [6, 113, 0.000471306256019766, 0.953194312519765],
-                           551: [6, 13, 0.0569350631190667, 0.7448746655457289],
-                           593: [2, 15, 0.0582544098560979, 0.5406510449527204],
-                           604: [8, 11, 0.0481795194440872, 0.7757600377716272]}
-
-        self.assertEqual(CoskyAlgorithme(self.r_big_reloaded), expected_values)
+        self.assertEqual(self.cosky_algo, self.expected_values)
 
 
-    if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__':
+    print("Running tests...")
+    unittest.main()
---
+++ Algorithms/Tests/DP_IDPTest.py
@@ -0,0 +1,33 @@
+import unittest
+import json
+
+from Algorithms.DP_IDP import DP_IDP
+
+class DP_IDPTest(unittest.TestCase):
+    """Test suite for DP_IDP algorithm"""
+
+    def setUp(self):
+        """Set up the test case"""
+        with open('../r_big.json', 'r') as f:
+            self.r_big = json.load(f)
+
+        # Excepted values for Dp_IDP algorithm
+        self.excepted_output = {31: 100.12794123202812,
+                                66: 4.814669361678212,
+                                88: 177.54031859048024,
+                                102: 64.33287037248346,
+                                149: 56.588694473078284,
+                                381: 19.348062785161932,
+                                398: 19.69864863692009,
+                                495: 24.734938700428767,
+                                540: 0.23856062735983122,
+                                550: 2.035309896463078,
+                                592: 3.960770184126881,
+                                603: 3.5719974892938056}
+        # Run DP_IDP algorithm
+        self.dpidp_values = DP_IDP(self.r_big).score
+
+
+    def test_Algo_DPIDP(self):
+        """Test the DP_IDP algorithm with a sample dataset"""
+        self.assertEqual(self.excepted_output, self.dpidp_values)
